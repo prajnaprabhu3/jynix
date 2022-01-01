db128d74513d006dfa2c05e5ae6a039d3b395308
Prajna <prajnapprabhu3@gmail.com>
2022-01-02 01:32:51 +0530

    analytics

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -1,11 +1,91 @@
-import React from 'react'
+import React from 'react';
+import { cardStyle } from './ReusableStyles';
+import styled from 'styled-components';
+import { BiDotsVerticalRounded } from 'react-icons/bi';
+import { CircularProgressbar } from 'react-circular-progressbar';
+// import 'react-circular-progressbar/dist/styles.css'; 
+
+
+
 
 function Analytics() {
-    return (
-        <div>
+    const percentage1 = 50 %;
+    <CircularProgressbar value={percentage1} text={`${percentage1}%`} />;
 
-        </div>
+    return (
+        <Section>
+            <div className="analytics">
+                <div className="firstRow">
+                    <div> <h4>Works</h4> </div>
+                    <div> <BiDotsVerticalRounded /></div>
+                </div>
+                <div className="secondRow">
+                    <div><h4>Ring</h4></div>
+                    <div><h2>75</h2>
+                        <p>Works Today</p>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+            <div className="analytics">
+                <div className="firstRow">
+                    <div> <h4>Analytics</h4> </div>
+                    <div> <BiDotsVerticalRounded /></div>
+                </div>
+                <div className="secondRow">
+                    <div><h4>Ring</h4></div>
+                    <div><h2>310</h2>
+                        <p>Analytics Today</p></div>
+                </div>
+            </div>
+
+
+            <div className="analytics">
+                <div className="firstRow">
+                    <div> <h4>Statistics</h4> </div>
+                    <div> <BiDotsVerticalRounded /></div>
+                </div>
+                <div className="secondRow">
+                    <div><h4>Ring</h4></div>
+                    <div><h2>75</h2>
+                        <p>Works Today</p></div>
+                </div>
+            </div>
+
+
+
+            <div className="analytics">
+                <div className="firstRow">
+                    <div> <h4>Tasks</h4> </div>
+                    <div> <BiDotsVerticalRounded /></div>
+                </div>
+                <div className="secondRow">
+                    <div><h4>Ring</h4></div>
+                    <div><h2>15</h2>
+                        <p>Task Today</p></div>
+                </div>
+            </div>
+
+
+
+        </Section>
     )
 }
 
 export default Analytics
+
+const Section = styled.section`
+display: grid;
+grid-template-columns: repeat(2,1fr);
+gap:1rem;
+
+.analytics{
+    ${cardStyle}:
+    // padding:1rem;
+    display:flex;
+    flex-direction:column;
+}
+`;
---
+++ src/components/ReusableStyles.jsx
@@ -0,0 +1,11 @@
+import { css } from "styled-components";
+
+export const cardStyle = css`
+// padding: 2.5rem 6rem 5rem 4rem;
+background-color:white;
+height:23vh;
+width:21vw;
+border-radius:0.8rem;
+
+
+`;
